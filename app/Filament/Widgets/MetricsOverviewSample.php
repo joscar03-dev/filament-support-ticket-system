8a6293987ca2d317a3208fe6dbf4d10eed727b2e
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-05-14 16:30:02 -0500

    creacin de widget a trtavez de metric personalizado

---
+++ app/Filament/Widgets/MetricsOverviewSample.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\CustomWidgets\MetricsOverviewWidget;
+use App\Filament\CustomWidgets\MetricWidgetSample;
+
+class MetricsOverviewSample extends MetricsOverviewWidget
+{
+    protected function getMetrics(): array
+    {
+        return [
+            MetricWidgetSample::class,
+        ];
+    }
+
+
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\CustomWidgets\MetricsOverviewWidget;
 use App\Filament\CustomWidgets\MetricWidget;
 use App\Filament\Widgets\TicketsOverviewChart;
 use Filament\Facades\Filament;
@@ -50,7 +51,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                MetricWidget::class,
                 /* Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class, */
 
