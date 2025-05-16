3a8e961c75e35e557b7a4a4c301a8453a737ff69
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-05-16 12:11:03 -0500

    agregar alheaderEnd de la lista tickets crear un nuevo ticket

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -1,4 +1,5 @@
-<x-filament::section icon="heroicon-o-rectangle-stack" icon-color="info" class="max-w-7xl mx-auto items-center justify-center">
+<x-filament::section icon="heroicon-o-rectangle-stack" icon-color="info"
+    class="max-w-7xl mx-auto items-center justify-center">
     <x-slot name="heading">
         {{ __('Lista de Tickets') }}
     </x-slot>
@@ -8,7 +9,9 @@
     </x-slot>
 
     <x-slot name="headerEnd">
-        headerEnd
+        <x-filament::button href="{{ route('tickets.create')}}" tag="a">
+            Nuevo Ticket
+        </x-filament::button>
     </x-slot>
 
 
